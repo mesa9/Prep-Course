e0dec8072530bdc8b4ff76378dac2baba858d30a
mesa9 <micorreo.mesa@gmail.com>
2022-12-09 19:05:32 -0500

    Update homework.js

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -17,13 +17,14 @@ function mayoriaDeEdad(edad) {
 }
   
 function conection(status) {
-  //Recibimos un estado de conexión de un usuario representado por un valor numérico. 
-  //Cuando el estado es igual a 1, el usuario está "Online"
-  //Cuando el estado es igual a 2, el usuario está "Away"
-  //De lo contrario, presumimos que el usuario está "Offline"
-  //Devolver el estado de conexión de usuario en cada uno de los casos.
+  If ( status === 1) {
+  return "online";
 }
-
+   else if (status === 2) {
+  return "away";
+}  else {return "offline"
+}
+}
 function saludo(idioma) {
   // Devuelve un saludo en tres diferentes lenguajes:
   // Si "idioma" es "aleman", devuelve "Guten Tag!"
