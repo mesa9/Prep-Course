e3feaf01d9e22878941006c393b8a1c5bf487d5f
mesa9 <micorreo.mesa@gmail.com>
2023-01-12 20:37:07 -0500

    Update homework.js

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,16 +1,11 @@
 <<<<<<< HEAD
-// En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
-// Crea una variable "string", puede contener lo que quieras:
 const nuevaString = 'hola';
 
-// Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 5;
  
-// Crea una variable booleana:
 const nuevoBool = true;
 
-// Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
 
 // Resuelve el siguiente problema matemático:
@@ -26,24 +21,20 @@ const nuevoModulo = 21 % 5 === 1;
 // No cambies los nombres de las funciones.
 
 function devolverString(str) {
-  // "Return" la string provista: str
-  // Tu código: 
+ 
   return str;
   
 }
 
 function suma(x, y) {
-  // "x" e "y" son números
-  // Suma "x" e "y" juntos y devuelve el valor
-<<<<<<< HEAD
-  // Tu código: 
+
+ 
 return (x + y);
   
 }
 
 function resta(x, y) {
-  // Resta "y" de "x" y devuelve el valor
-  // Tu código:
+  
   return (x - y);
 }
 
